0e5ec3ced946e450a60898c06ee4c6dd85e23f10
Bruno Nakahara <Bruno_Nakahara95@yahoo.com>
2022-04-06 23:44:22 -0300

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 //Criando axios com os parâmetros necessário para realizar a requisição
 const axiosAPI = axios.create({
-  baseURL: 'http://192.168.0.10:3000/v1', //Colocar o ip da máquina no lugar do localhost, pois react native não reconhece localhost
+  baseURL: 'http://XXXX:3000/v1', //Colocar o ip da máquina no lugar do localhost, pois react native não reconhece localhost
   headers: {
     'Content-Type': 'application/json', //O corpo da requisição vai estar no formato JSON
   },
